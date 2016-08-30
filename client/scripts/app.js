cc99424aaab4a460ce5548f0e255e9752a6d7a6d
William Simmons <willsimmons@gmail.com>
2016-08-30 15:00:14 -0700

    added button, added rooms and friends arrays that are refreshed each time the chats are refresshed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  
+  var friends = [];
+  var rooms = [];
   //app initialization
   
   
@@ -13,66 +14,91 @@ $(document).ready(function() {
     app.send(message);
   });
 
-  
+  $("#clear").click(function(event){
+    event.preventDefault();
+    app.clearMessages();
+  });
 
-$('#chats').on('click', '.friend', function(){ 
-  console.log(this);
-});
+  $('#chats').on('click', '.friend', function(){ 
+    console.log(this);
+  });
 
-var app={
-  
-  fetch: function(){
-    $.ajax({
-      url: "https://api.parse.com/1/classes/chatterbox",
-      type: 'GET',
-      contentType: "appication/json; charset=utf-8",
-      data: {
-        order:"-createdAt"
-      },
-      success: function(data) {
+  var app={
+
+    clearMessages: function(){
+     $("#chats").children().remove();
+    },
+
+    fetch: function(){
+      $.ajax({
+        url: "https://api.parse.com/1/classes/chatterbox",
+        type: 'GET',
+        contentType: "appication/json; charset=utf-8",
+        data: {
+          order:"-createdAt"
+        },
+        success: function(data) {
+        // changes just use data.results;
         var JSONObject= data;
-        app.addMessage(JSONObject);
+        app.addMessage(JSONObject,friends,rooms);
       },
       error: function(data, textStatus, error) {
         console.log('error: ', data, 'data', textStatus, 'error', error);
       }
     });
-  },
+    },
 
-  send: function(message){
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'POST',
-      data: JSON.stringify(message),
-      dataType: "JSON",
-      success: function (data) {
-        console.log('chatterbox: Message sent'); 
+    send: function(message){
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(message),
+        dataType: "JSON",
+        success: function (data) {
+          console.log('chatterbox: Message sent'); 
+        //automatically refresh when new message is added
+        app.fetch();
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message');
       }
     });
+    },
+
+    init: function(){
+      app.fetch();
+    },
+
+    addMessage: function(message,friends,rooms){
+      var cleanString;
+      $.each(message.results, function(index, object){
+        $.each(object, function(){
+          console.log(object);
+          cleanString = object.text;
+          if(cleanString !== undefined){
+            cleanString.replace(/^[0-9a-zA-Z]{1,16}$/);
+          }
+          cleanString = '<div class="friend"><strong>'+object.username+ "</strong>"  + ": " + cleanString+ "   " + object.createdAt+"</div>";
+          friends.push(object.username);
+          rooms.push(object.roomname);
+        });
+        $("#chats").append(cleanString);
+      });
+      app.cleanRooms(friends,rooms);
+    },
+
+  //get only one copy of each room and each friend in the messages div
+  cleanRooms: function (friends,rooms){
+    friends = _(friends).unique();
+    rooms =_(rooms).unique();
+    //put all the rooms in the drop down menu
+    console.log(rooms);
+    console.log(friends);
   },
 
-  init: function(){
-    app.fetch();
+  addRoom: function(roomName) {
+
   },
-  
-  addMessage: function(message){
-    var cleanString;
-    $.each(message.results, function(index, object){
-      $.each(object, function(){
-        cleanString = object.text;
-        if(cleanString !== undefined){
-          cleanString.replace(/^[0-9a-zA-Z]{1,16}$/);
-        }
-        cleanString = '<div class="friend"><strong>'+object.username+ "</strong>"  + ": " + cleanString+ "   " + object.createdAt+"</div>"    
-      });
-      $("#chats").append(cleanString);
-    });
-  },
-  
-  addRoom: function(roomName) {},
   
   addFriend: function(userName) {
     $(".friends").append("<div>" + userName + "</div>")
@@ -81,5 +107,6 @@ var app={
   handleSubmit: function(){}
 
 };
+
 app.init();
 });
